203f99f7d98625cff16d9b3311d4770319b338b1
snoopy <acgdigital0430@gmail.com>
2022-04-10 21:07:46 +0800

    update server route

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('nikedb.json');
+const pumaRouter =jsonServer.router('pumadb.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+server.use(pumaRouter);
 
 server.listen(port);
