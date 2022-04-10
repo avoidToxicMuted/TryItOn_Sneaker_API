f6fce9e59ce8d9e4dcbe66f52d03580be1cec7cb
snoopy <acgdigital0430@gmail.com>
2022-04-10 21:28:30 +0800

    added sneakerdb controller

---
+++ sneakerdb.js
@@ -0,0 +1,14 @@
+var nikeRoute  = require('./nikedb.json');
+var pumaRoute = require('./pumadb.json');
+var adidasRoute  = require('./adidasdb.json');
+var hermesRoute = require('./hermesdb.json');
+// and so on
+
+module.exports = function() {
+return {
+firstRoute  : firstRoute,
+secondRoute : secondRoute,
+thirdRoute  : thirdRoute,
+fourthRoute : fourthRoute
+ }
+}
