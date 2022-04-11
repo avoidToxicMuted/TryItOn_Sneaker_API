32a719c6259e49eb15f1ed2b47f5e871a8bfd06e
snoopy <acgdigital0430@gmail.com>
2022-04-11 09:20:47 +0800

    update db struct

--- sneakerdb.js
+++ sneakerdb.js
@@ -1,13 +1,11 @@
 var nikeRoute  = require('./nikedb.json');
 var pumaRoute = require('./pumadb.json');
-// var adidasRoute  = require('./adidasdb.json');
-// var hermesRoute = require('./hermesdb.json');
 // and so on
 
 module.exports = function() {
 return {
-firstRoute  : nikeRoute,
-secondRoute : pumaRoute
+nike  : nikeRoute,
+puma : pumaRoute
 // thirdRoute  : thirdRoute,
 // fourthRoute : fourthRoute
  }
