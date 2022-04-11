34f0ed51418bfa27033b1af89139ffee9c6c0b34
snoopy <acgdigital0430@gmail.com>
2022-04-11 08:57:11 +0800

    updated nike db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(require('./sneakerdb.js')());
-// const pumaRouter =jsonServer.router('pumadb.json');
+// const router = jsonServer.router(require('./sneakerdb.js')());
+const router =jsonServer.router('pumadb.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
