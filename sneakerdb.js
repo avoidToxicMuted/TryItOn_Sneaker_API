60ded9a2649d6dab1de69ef7dfbb6cc2d6fbd6a8
snoopy <acgdigital0430@gmail.com>
2022-04-11 12:23:40 +0800

    added hermes and adidas

--- sneakerdb.js
+++ sneakerdb.js
@@ -1,12 +1,14 @@
+var adidasRoute  = require('./adidas.json');
 var nikeRoute  = require('./nikedb.json');
 var pumaRoute = require('./pumadb.json');
+var hermesRoute = require('./hermes.json');
 // and so on
 
 module.exports = function() {
 return {
+adidas  : adidasRoute,
 nike  : nikeRoute,
-puma : pumaRoute
-// thirdRoute  : thirdRoute,
-// fourthRoute : fourthRoute
+puma : pumaRoute,
+hermes : hermesRoute
  }
 }
