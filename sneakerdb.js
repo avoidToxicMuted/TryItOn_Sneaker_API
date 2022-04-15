fe64ff5118f38ac93cce51b9b499720e7569ab67
snoopy <acgdigital0430@gmail.com>
2022-04-15 16:32:42 +0800

    update the server for the locationRoute

--- sneakerdb.js
+++ sneakerdb.js
@@ -3,7 +3,6 @@ var nikeRoute  = require('./nikedb.json');
 var pumaRoute = require('./pumadb.json');
 var hermesRoute = require('./hermes.json');
 var locationRoute = require('./storeLocationdb.json');
-// and so on
 
 module.exports = function() {
 return {
